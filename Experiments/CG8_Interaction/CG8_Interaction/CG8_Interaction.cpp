4d13f37b18d6d78c744f50b23c6970b2a2577381
wpxu08 <wpxu08@gmail.com>
2019-02-25 11:45:15 +0800

    CG8_Interaction

--- Experiments/CG8_Interaction/CG8_Interaction/CG8_Interaction.cpp
+++ Experiments/CG8_Interaction/CG8_Interaction/CG8_Interaction.cpp
@@ -1,139 +1,64 @@
+#define NDEBUG
 #include <GL/glut.h>
-
 #include <math.h>
-
 #include <stdlib.h>
+#include <stdio.h>
 
-#define DEGREES_TO_RADIANS 3.14159/180.0
-
-static GLfloat spin = 0.0;
-
-GLfloat x = 0.0;
-
-GLfloat y = 0.0;
-
+GLfloat x = 50.0;
+GLfloat y = 50.0;
 GLfloat size = 50.0;
-
-GLfloat angle = 2.0;
-
 GLsizei wh = 500, ww = 500;
-
-void square()
-
-{
-
-	glBegin(GL_QUADS);
-
-	glVertex2f(x,y);
-
-	glVertex2f(-y,x);
-
-	glVertex2f(-x,-y);
-
-	glVertex2f(y,-x);
-
+void drawSquare(GLint x, GLint y) {
+	y = wh - y;
+	glColor3f(1.0, 1.0, 0.0);
+	glBegin(GL_POLYGON);
+	glVertex3f(x + size, y + size, 0);
+	glVertex3f(x - size, y + size, 0);
+	glVertex3f(x - size, y - size, 0);
+	glVertex3f(x + size, y - size, 0);
 	glEnd();
-
 }
-
-void spinDisplay(void)
-
-{
-
-	spin = spin + 2.0;
-
-	if (spin > 360.0) spin = spin - 360.0;
-
-	x=125.0 * cos(DEGREES_TO_RADIANS*spin);
-
-	y=125.0 * sin(DEGREES_TO_RADIANS*spin);
-
-	glutPostRedisplay();
-
-}
-
 void mydisplay()
-
 {
-
 	glClear(GL_COLOR_BUFFER_BIT);
-
-	glColor3f(1.0, 1.0, 1.0);
-
-	square();
-
+	drawSquare(x, y);
 	glutSwapBuffers();
-
+	glutPostRedisplay();
 }
-
 void init()
-
 {
-
-	glClearColor (0.0, 0.0, 0.0, 1.0);
-
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	printf("Click Mouse Left Button to Move the Square");
 }
-
 void myreshape(GLint w, GLint h) {
-
 	glViewport(0, 0, w, h);
-
 	glMatrixMode(GL_PROJECTION);
-
 	glLoadIdentity();
-
-	glOrtho(-w/2, w/2, -h/2, h/2, -1.0, 1.0);
-
+	glOrtho(0, w, 0, h, -1.0, 1.0);
 	glMatrixMode(GL_MODELVIEW);
-
 	ww = w;
-
 	wh = h;
-
 }
-
 void mymouse(GLint button, GLint state, GLint wx, GLint wy)
-
 {
-
-	if(button ==GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
-
+	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
 		exit(0);
-
-	if(button ==GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-
-		glutIdleFunc(spinDisplay);
-
-	if(button== GLUT_MIDDLE_BUTTON && state == GLUT_DOWN)
-
-		glutIdleFunc(NULL);
-
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		x = wx;
+		y = wy;
+	}
 }
-
 void main(int argc, char** argv)
-
 {
-
-	glutInit(&argc,argv);
-
+	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-
-	glutInitWindowSize(500,500);
-
-	glutInitWindowPosition(0,0);
-
-	glutCreateWindow("double");
-
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Move the Square");
 	init();
-
 	glutDisplayFunc(mydisplay);
-
 	glutReshapeFunc(myreshape);
-
 	glutMouseFunc(mymouse);
-
-	glutIdleFunc(spinDisplay);
-
 	glutMainLoop();
-
 }
--- Experiments/CG8_Interaction2/CG8_Interaction2/Interaction2.cpp
+++ Experiments/CG8_Interaction2/CG8_Interaction2/Interaction2.cpp
@@ -1,119 +1,74 @@
+#define NDEBUG
 #include <GL/glut.h>
+ #include <math.h>
+ #include <stdlib.h>
+ #define DEGREES_TO_RADIANS 3.14159/180.0
 
-#include <math.h>
+ static GLfloat spin = 0.0;
+ GLfloat x = 0.0;
+ GLfloat y = 0.0;
+ GLfloat size = 50.0;
+ GLfloat angle = 2.0;
+ GLsizei wh = 500, ww = 500;
 
-#include <stdlib.h>
-
-GLfloat x = 0.0;
-
-GLfloat y = 0.0;
-
-GLfloat size = 50.0;
-
-GLsizei wh = 500, ww = 500;
-
-void drawSquare(GLint x, GLint y) {
-
-	y = wh-y;
-
-	glBegin(GL_POLYGON);
-
-	glVertex3f(x + size, y + size, 0);
-
-	glVertex3f(x - size, y + size, 0);
-
-	glVertex3f(x - size, y - size, 0);
-
-	glVertex3f(x + size, y - size, 0);
-
-	glEnd();
-
-}
-
-void mydisplay()
-
-{
-
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	glColor3f(1.0, 1.0, 1.0);
-
-	drawSquare(x, y);
-
-	glutSwapBuffers();
-
-	glutPostRedisplay();
-
-}
-
-void init()
-
-{
-
-	glClearColor (0.0, 0.0, 0.0, 1.0);
-
-}
-
-void myreshape(GLint w, GLint h) {
-
-	glViewport(0, 0, w, h);
-
-	glMatrixMode(GL_PROJECTION);
-
-	glLoadIdentity();
-
-	glOrtho(0, w, 0, h, -1.0, 1.0);
-
-	glMatrixMode(GL_MODELVIEW);
-
-	ww = w;
-
-	wh = h;
-
-}
-
-void mymouse(GLint button, GLint state, GLint wx, GLint wy)
-
-{
-
-	if(button ==GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
-
-		exit(0);
-
-	if(button ==GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-
-	{
-
-		x = wx;
-
-		y = wy;
-
-	}
-
-}
-
-void main(int argc, char** argv)
-
-{
-
-	glutInit(&argc,argv);
-
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-
-	glutInitWindowSize(500,500);
-
-	glutInitWindowPosition(0,0);
-
-	glutCreateWindow("double");
-
-	init();
-
-	glutDisplayFunc(mydisplay);
-
-	glutReshapeFunc(myreshape);
-
-	glutMouseFunc(mymouse);
-
-	glutMainLoop();
-
-}
+ void square()
+ {
+ 	glBegin(GL_QUADS);
+ 	glVertex2f(x,y);
+ 	glVertex2f(-y,x);
+ 	glVertex2f(-x,-y);
+ 	glVertex2f(y,-x);
+ 	glEnd();
+ }
+ void spinDisplay(void)
+ {
+ 	spin = spin + 2.0f;
+ 	if (spin > 360.0f) 
+		spin = spin - 360.0f;
+ 	x=125.0f * cos(DEGREES_TO_RADIANS*spin);
+ 	y=125.0f * sin(DEGREES_TO_RADIANS*spin);
+ 	glutPostRedisplay();
+ }
+ void mydisplay()
+ {
+ 	glClear(GL_COLOR_BUFFER_BIT);
+ 	glColor3f(1.0, 1.0, 1.0);
+ 	square();
+ 	glutSwapBuffers();
+ }
+ void init()
+ {
+ 	glClearColor (0.0, 0.0, 0.0, 1.0);
+ }
+ void myreshape(GLint w, GLint h) {
+ 	glViewport(0, 0, w, h);
+ 	glMatrixMode(GL_PROJECTION);
+ 	glLoadIdentity();
+ 	glOrtho(-w/2, w/2, -h/2, h/2, -1.0, 1.0);
+ 	glMatrixMode(GL_MODELVIEW);
+ 	ww = w;
+ 	wh = h;
+ }
+ void mymouse(GLint button, GLint state, GLint wx, GLint wy)
+ {
+ 	if(button ==GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
+ 		exit(0);
+ 	if(button ==GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+ 		glutIdleFunc(spinDisplay);
+ 	if(button== GLUT_MIDDLE_BUTTON && state == GLUT_DOWN)
+ 		glutIdleFunc(NULL);
+ }
+ void main(int argc, char** argv)
+ {
+ 	glutInit(&argc,argv);
+ 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+ 	glutInitWindowSize(500,500);
+ 	glutInitWindowPosition(0,0);
+ 	glutCreateWindow("double");
+ 	init();
+ 	glutDisplayFunc(mydisplay);
+ 	glutReshapeFunc(myreshape);
+ 	glutMouseFunc(mymouse);
+ 	glutIdleFunc(spinDisplay);
+ 	glutMainLoop();
+ }
